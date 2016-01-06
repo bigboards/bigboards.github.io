861504c81b47397dd41740ba38b815319e50c2cd
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2016-01-06 19:46:13 +0100

    First take on toolbar

--- _js/bigboards.js
+++ _js/bigboards.js
@@ -8,10 +8,18 @@
             'ngMaterial'
         ])
         .config(function ($mdThemingProvider) {
+
+            $mdThemingProvider.definePalette('bigboards',
+                $mdThemingProvider.extendPalette('blue-grey', {
+                    '50': '008888'
+                })
+            );
+
             $mdThemingProvider.theme('default')
-                .primaryPalette('indigo')
-                .accentPalette('grey')
-                .warnPalette('red');
+                .primaryPalette('blue-grey')
+                .accentPalette('teal')
+                .warnPalette('deep-orange')
+            ;
         })
         .controller('bigboardsController', ['$scope', function ($scope) {
             $scope.currentYear = new Date().getFullYear();
