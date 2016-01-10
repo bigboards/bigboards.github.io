830b76a08ea6e599b6370e4e94c11a2ccda9c75d
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2016-01-10 08:13:19 +0100

    Finally a decent looking menu!

--- _js/bigboards.a.module.js
+++ _js/bigboards.a.module.js
@@ -13,15 +13,13 @@
     theme.$inject = ['$mdThemingProvider'];
 
     function theme($mdThemingProvider) {
-
-        $mdThemingProvider.definePalette('bigboards',
-            $mdThemingProvider.extendPalette('blue-grey', {
-                '50': '008888'
-            })
-        );
+        var bbPrimary = $mdThemingProvider.extendPalette('teal', {
+            '500': '00aaaa'
+        });
+        $mdThemingProvider.definePalette('bbPrimary', bbPrimary);
 
         $mdThemingProvider.theme('default')
-            .primaryPalette('blue-grey')
+            .primaryPalette('bbPrimary')
             .accentPalette('teal')
             .warnPalette('deep-orange')
         ;
